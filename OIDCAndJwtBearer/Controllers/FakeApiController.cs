36351b7f402db76b00b139985ecf8459d185d4b2
Michael Christensen <mac@seges.dk>
2022-12-20 15:26:52 +0100

    Sample

---
+++ OIDCAndJwtBearer/Controllers/FakeApiController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OIDCAndJwtBearer.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class FakeApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Json(this.HttpContext.User.Claims.Select(c => new Tuple<string,string>(c.Type, c.Value)));
+        }
+    }
+}
